154012dd53ae93f2a7ab0fe45663a8c749d48c5a
Callum Axon <callumaxon@gmail.com>
2019-05-02 20:09:58 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -17,29 +17,29 @@ class StudentsController extends Controller
 
     public function show($id)
     {
-    	try {
-    		$student = Student::findOrFail($id)->load('course');
-    	} catch (ModelNotFoundException $exception) {
-			return response()->json(['error' => 'Student not found.']);
-    	}
+        try {
+            $student = Student::findOrFail($id)->load('course');
+        } catch (ModelNotFoundException $exception) {
+            return response()->json(['error' => 'Student not found.']);
+        }
 
-    	return response()->json(['data' => ['student' => $student]]);
+        return response()->json(['data' => ['student' => $student]]);
     }
 
     public function store(Request $request)
     {
-    	$data = $this->validate($request, [
-			'identifier' => 'required|string|unique:students,identifier',
-			'name_first' => 'required|string',
-			'name_last' => 'required|string',
-			'course_id' => 'required',
-			'dob' => 'required',
-			'gender' => 'required',
-			'start_year' => 'required'
-    	]);
+        $data = $this->validate($request, [
+            'identifier' => 'required|string|unique:students,identifier',
+            'name_first' => 'required|string',
+            'name_last'  => 'required|string',
+            'course_id'  => 'required',
+            'dob'        => 'required',
+            'gender'     => 'required',
+            'start_year' => 'required',
+        ]);
 
-    	$student = Student::create($data);
+        $student = Student::create($data);
 
-    	return response()->json(['success' => 'Student Created'], 201)->header('Location', route('students.show', ['id' => $student->id]));
+        return response()->json(['success' => 'Student Created'], 201)->header('Location', route('students.show', ['id' => $student->id]));
     }
 }
