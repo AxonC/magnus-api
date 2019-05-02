60223dd67221a96758916c7795cf20539e9acae1
Callum Axon <callumaxon@gmail.com>
2019-05-02 19:08:54 +0100

    merge

--- app/Student.php
+++ app/Student.php
@@ -7,9 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     protected $guarded = [];
+    protected $primaryKey = 'internal_identifier';
 
     public function course()
     {
         return $this->belongsTo(Course::class);
+    }
+
+    public function person()
+    {
+    	return $this->belongsTo(Person::class, 'identifier');
+    }
+
+    public function getLastPositionAttribute()
+    {
+
     }
 }
