061df7b2a632ba2ead4c96ed4fbc0079fc4cbe55
Callum Axon <callumaxon@gmail.com>
2019-05-04 15:44:05 +0000

    Apply fixes from StyleCI

--- routes/web.php
+++ routes/web.php
@@ -87,7 +87,6 @@ $router->group(['middleware' => 'auth'], function () use ($router) {
         ['as' => 'security.alert.mark', 'uses' => 'SecurityAlertsController@update']);
     $router->get('alerts/camera/{camera}',
         ['as' => 'security.alert.show.camera', 'uses' => 'SecurityAlertsController@byCamera']);
-
 });
 
 $router->group(['middleware' => 'camera'], function () use ($router) {
