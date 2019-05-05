cfc09221f19c04aa923f1d7bada0435416818e34
Callum Axon <callumaxon@gmail.com>
2019-05-05 22:12:56 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/DashboardStatusController.php
+++ app/Http/Controllers/DashboardStatusController.php
@@ -11,22 +11,21 @@ class DashboardStatusController extends Controller
 {
     public function index()
     {
-    	return response()->json([
-    		'data' => [
-    			'totalEnrolled' => Student::count(),
-    			'recentNewEnrolled' => Student::where('created_at', '>', \Carbon\Carbon::parse('2 days ago'))->get()->count(),
-    			'validDetections' => PositionReport::count(),
-    			'invalidDetections' => SecurityAlert::count(),
-                'cameras' => Camera::withCount('reports', 'alerts')->get()->each(function (Camera $model) {
+        return response()->json([
+            'data' => [
+                'totalEnrolled'     => Student::count(),
+                'recentNewEnrolled' => Student::where('created_at', '>', \Carbon\Carbon::parse('2 days ago'))->get()->count(),
+                'validDetections'   => PositionReport::count(),
+                'invalidDetections' => SecurityAlert::count(),
+                'cameras'           => Camera::withCount('reports', 'alerts')->get()->each(function (Camera $model) {
                     $success_rate = 0;
-                    if ($model->alerts_count > 1 && $model->reports_count > 1)
-                    {
+                    if ($model->alerts_count > 1 && $model->reports_count > 1) {
                         $success_rate = $model->alerts_count / $model->reports_count;
                     }
 
                     return $model->setAttribute('success_rate', $success_rate);
                 }),
-            ]
-    	]);
+            ],
+        ]);
     }
 }
